5e0ca701289ae227e2bab47eb35aa0e8b273fbba
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-29 14:46:47 -0600

    Updates

---
+++ precache-manifest.3b2e71d757b82ce3abbe2b8836ac16a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad179db5962c6e5888e4ac1bf5c6e99",
+    "url": "/music-app/index.html"
+  },
+  {
+    "revision": "bdd2cbb18eec1f81d570",
+    "url": "/music-app/static/css/main.ece4796f.chunk.css"
+  },
+  {
+    "revision": "49a1fd5aba39d1ae84ff",
+    "url": "/music-app/static/js/2.7f134575.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/music-app/static/js/2.7f134575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdd2cbb18eec1f81d570",
+    "url": "/music-app/static/js/main.bd44f5c3.chunk.js"
+  },
+  {
+    "revision": "4f04cc2715f387e37000",
+    "url": "/music-app/static/js/runtime-main.726d910a.js"
+  }
+]);
